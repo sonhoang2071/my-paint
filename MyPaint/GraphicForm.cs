e0a90fea80c8dc516028fbe3d6e02bae2a10da69
Admin <Admin@DESKTOP-ET9T8OJ>
2024-01-17 15:19:07 +0700

    Add project files.

---
+++ MyPaint/GraphicForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPaint
+{
+    public partial class GraphicForm : Form
+    {
+        public GraphicForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GraphicForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
